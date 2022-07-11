895a1923e4d057960cd86d7122115173aee08caf
Joel Rivera <rivera@joel.mx>
2022-07-11 01:37:19 -0500

    Update dependencies add some improvements for the nix structure

--- default.nix
+++ default.nix
@@ -1,55 +1,20 @@
-{ pkgs ? (import ./nixpkgs.nix {}).pkgs }:
+{ }:
 let
-  inherit(pkgs) poetry2nix;
-
-  python = pkgs.python38;
-
+  sources = import ./nix/sources.nix {};
+  pkgs = import sources.nixpkgs {};
+  poetry2nix = pkgs.callPackage sources.poetry2nix { inherit pkgs; } ;
+  python = pkgs.python310;
   projectDir = ./.;
-
-  overrides = poetry2nix.overrides.withDefaults (
-    self: super: {
-      # disable the removal of pyproject.toml, required because of setuptools_scm
-      # in the following packages.
-      jaraco-functools = super.jaraco-functools.overridePythonAttrs (
-        old: {
-          dontPreferSetupPy = true;
-        }
-      );
-
-      tempora = super.tempora.overridePythonAttrs (
-        old: {
-          dontPreferSetupPy = true;
-        }
-      );
-
-      portend = super.portend.overridePythonAttrs (
-        old: {
-          dontPreferSetupPy = true;
-        }
-      );
-
-    }
-  );
-
   app = poetry2nix.mkPoetryApplication {
-    inherit python projectDir overrides;
+    inherit python projectDir;
   };
-
   env = poetry2nix.mkPoetryEnv {
     editablePackageSources = {
         maki = ./.;
     };
-    inherit python projectDir overrides;
+    inherit python projectDir;
   };
 in {
-  app = app.dependencyEnv.overrideAttrs (
-    old:  {
-      name = "maki-${ app.version }";
-    }
-  );
-  shell = pkgs.mkShell {
-    buildInputs = [ env pkgs.glibcLocales ];
-  };
-
-  inherit env;
+  inherit app env;
+  shell = pkgs.mkShell { buildInputs = [ env pkgs.glibcLocales ]; };
 }
