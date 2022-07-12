8fbf376d9b5c56327f4d05956931f0a97ec603c7
Joel Rivera <rivera@joel.mx>
2022-07-12 02:39:23 -0500

    Version bump and define a stable path to get the static files in the nixpkg

--- default.nix
+++ default.nix
@@ -5,9 +5,14 @@ let
   poetry2nix = pkgs.callPackage sources.poetry2nix { inherit pkgs; } ;
   python = pkgs.python310;
   projectDir = ./.;
-  app = poetry2nix.mkPoetryApplication {
+  app = (poetry2nix.mkPoetryApplication {
     inherit python projectDir;
-  };
+    # creat a symlink for a constant path for the static files (in case that we change python versions)
+    postInstall = ''
+       mkdir -p  $out/var/lib/static
+       ln -s $out/lib/${ python.libPrefix }/site-packages/maki/static  $out/var/lib/static/maki
+    '';
+  }).dependencyEnv.overrideAttrs (old: {name = "maki-0.4.0"; });
   env = poetry2nix.mkPoetryEnv {
     editablePackageSources = {
         maki = ./.;
